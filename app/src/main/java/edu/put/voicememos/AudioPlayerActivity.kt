0ff4679c56c9d39eae6d01d797f0fb99df11f3ab
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-06-10 18:24:42 +0200

    minor fixes, toolbar

--- app/src/main/java/edu/put/voicememos/AudioPlayerActivity.kt
+++ app/src/main/java/edu/put/voicememos/AudioPlayerActivity.kt
@@ -1,22 +1,39 @@
 package edu.put.voicememos
 
 import android.media.MediaPlayer
+import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageButton
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.postDelayed
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.chip.Chip
 
 class AudioPlayerActivity : AppCompatActivity() {
 
     private lateinit var mediaPlayer: MediaPlayer
 
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var tvFilename: TextView
+
     private lateinit var btnPlay : ImageButton
     private lateinit var btnBackward : ImageButton
     private lateinit var btnForward : ImageButton
     private lateinit var speedChip : Chip
     private lateinit var seekBar : SeekBar
+
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+    private var delay = 1000L
+    private var jumpValue = 1000
+
+    private var playbackSpeed = 1.0f
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +43,24 @@ class AudioPlayerActivity : AppCompatActivity() {
         var filePath = intent.getStringExtra("filepath")
         var fileName = intent.getStringExtra("filename")
 
+        toolbar = findViewById(R.id.toolbar)
+        tvFilename = findViewById(R.id.tvFilename)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        toolbar.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
+        tvFilename.text = fileName
+
         mediaPlayer = MediaPlayer()
         mediaPlayer.apply {
             setDataSource(filePath)
             prepare()
         }
+
 
         btnBackward = findViewById(R.id.btnBackward)
         btnForward = findViewById(R.id.btnForward)
@@ -38,19 +68,74 @@ class AudioPlayerActivity : AppCompatActivity() {
         speedChip = findViewById(R.id.chip)
         seekBar = findViewById(R.id.seekBar)
 
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer.currentPosition
+            handler.postDelayed(runnable, delay)
+        }
+
         btnPlay.setOnClickListener{
             playPausePlayer()
         }
+        playPausePlayer()
+        seekBar.max = mediaPlayer.duration
+
+        mediaPlayer.setOnCompletionListener {
+            btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
+            handler.removeCallbacks(runnable)
+        }
+
+        btnForward.setOnClickListener{
+            mediaPlayer.seekTo(mediaPlayer.currentPosition + jumpValue)
+            seekBar.progress += jumpValue
+        }
+
+        btnBackward.setOnClickListener{
+            mediaPlayer.seekTo(mediaPlayer.currentPosition - jumpValue)
+            seekBar.progress -= jumpValue
+        }
+
+        speedChip.setOnClickListener{
+            if(playbackSpeed != 2f)
+                playbackSpeed += 0.5f
+            else
+                playbackSpeed = 0.5f
+
+            mediaPlayer.playbackParams = PlaybackParams().setSpeed(playbackSpeed)
+            speedChip.text = "x $playbackSpeed"
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if(p2)
+                    mediaPlayer.seekTo(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+
     }
 
     private fun playPausePlayer(){
         if(!mediaPlayer.isPlaying){
             mediaPlayer.start()
             btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause_circle, theme)
+            handler.postDelayed(runnable, delay)
         }else{
             mediaPlayer.pause()
             btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
+            handler.removeCallbacks(runnable)
 
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        handler.removeCallbacks(runnable)
     }
 }
