9b88b39f4db3e334ad88224b2557c04d138b1e3a
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-06-11 15:22:50 +0200

    firebase integration + resumebtn changed to restart

--- app/src/main/java/edu/put/voicememos/MainActivity.kt
+++ app/src/main/java/edu/put/voicememos/MainActivity.kt
@@ -1,14 +1,17 @@
 package edu.put.voicememos
 
 import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.ContextWrapper
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
@@ -17,6 +20,11 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.room.Room
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.storageMetadata
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
@@ -30,7 +38,6 @@ class MainActivity : AppCompatActivity() {
     private val MICROPHONE_PERMISSION_CODE = 200
     private var mediaRecorder: MediaRecorder? = null
     private var mediaPlayer: MediaPlayer? = null
-    private lateinit var fileNameEditText: EditText
     private lateinit var btnStop: ImageButton
     private lateinit var btnResume: ImageButton
     private lateinit var btnSave: ImageButton
@@ -56,14 +63,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var db : AppDatabase
 
+    private lateinit var storageReference: StorageReference
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mAuth = FirebaseAuth.getInstance()
+
+
         tvTimer = findViewById(R.id.tvTimer)
         waveformView = findViewById(R.id.waveformView)
 
-        fileNameEditText = findViewById(R.id.editTextFileName)
         btnStop = findViewById(R.id.btnStop)
         btnResume = findViewById(R.id.btnResume)
         btnSave = findViewById(R.id.btnSave)
@@ -81,8 +93,6 @@ class MainActivity : AppCompatActivity() {
         btnCancel = findViewById(R.id.btnCancel)
         btnOk = findViewById(R.id.btnOk)
 
-        fileNameEditText.visibility = View.GONE // Initially hide the file name input field
-
         if (isMicrophonePresent()) {
             getMicrophonePermission()
         }
@@ -96,7 +106,6 @@ class MainActivity : AppCompatActivity() {
         btnCancel.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
             bottomSheetBG.visibility = View.GONE
-            fileNameEditText.visibility = View.GONE
 
             // Hide the keyboard
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -112,7 +121,6 @@ class MainActivity : AppCompatActivity() {
             saveRecording(fileName)
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
             bottomSheetBG.visibility = View.GONE
-            fileNameEditText.visibility = View.GONE
 
             btnStop.visibility = View.GONE
             btnResume.visibility = View.GONE
@@ -125,6 +133,7 @@ class MainActivity : AppCompatActivity() {
             // Hide the keyboard
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(filenameInput.windowToken, 0)
+            uploadRecording()
         }
 
         filenameInput.setOnClickListener {
@@ -134,6 +143,7 @@ class MainActivity : AppCompatActivity() {
         btnList.setOnClickListener{
             startActivity(Intent(this, GalleryActivity::class.java))
         }
+
 
     }
 
@@ -158,9 +168,9 @@ class MainActivity : AppCompatActivity() {
         try {
             mediaRecorder = MediaRecorder().apply {
                 setAudioSource(MediaRecorder.AudioSource.MIC)
-                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
                 setOutputFile(tempFilePath) // Save to a temporary file
-                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
                 prepare()
                 start()
             }
@@ -186,36 +196,56 @@ class MainActivity : AppCompatActivity() {
             elapsedTime += System.currentTimeMillis() - startTime
             btnStop.visibility = View.GONE
             btnResume.visibility = View.VISIBLE
-            btnResume.setImageResource(R.drawable.ic_resume)
-            Toast.makeText(this, "Recording stopped. You can resume or save it.", Toast.LENGTH_LONG).show()
+            btnResume.setImageResource(R.drawable.ic_restart)
+            Toast.makeText(this, "Recording stopped. You can restart or save it.", Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
     fun btnResumePressed(v: View) {
-
         try {
             mediaRecorder?.apply {
+                // Resetowanie MediaRecorder, aby przygotować go do ponownego użycia
                 reset()
+
+                // Ustawienie ustawień MediaRecorder na te same, co wcześniej
                 setAudioSource(MediaRecorder.AudioSource.MIC)
-                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                setOutputFile(tempFilePath) // Continue with the same temporary file
-                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
+                setOutputFile(tempFilePath)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
                 prepare()
                 start()
             }
-            isRecordingStopped = false
+
+            // Resetowanie zegara i innych zmiennych
+            elapsedTime = 0L
             startTime = System.currentTimeMillis()
+
+            handler.removeCallbacks(timerRunnable)
+            handler.removeCallbacks(amplitudeRunnable)
             handler.post(timerRunnable)
             handler.post(amplitudeRunnable)
+
+            isRecordingStopped = false
+
+            // Ukrycie przycisku restartu
             btnResume.visibility = View.GONE
+
+            // Wyswietlenie przycisku stopu
             btnStop.visibility = View.VISIBLE
-            Toast.makeText(this, "Recording resumed", Toast.LENGTH_LONG).show()
+
+            // Zresetowanie amplitud i zegara
+            tvTimer.text = "00:00:00"
+            waveformView.clearAmplitudes()
+
+            Toast.makeText(this, "Restarting recording", Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
+
 
     fun btnDeletePressed(v: View) {
         try {
@@ -250,7 +280,6 @@ class MainActivity : AppCompatActivity() {
             btnSave.visibility = View.GONE
             btnRecord.visibility = View.VISIBLE
             btnList.visibility = View.VISIBLE
-            fileNameEditText.visibility = View.GONE
 
             Toast.makeText(this, "Recording deleted", Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
@@ -270,41 +299,6 @@ class MainActivity : AppCompatActivity() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         bottomSheetBG.visibility = View.VISIBLE
         filenameInput.setText(defaultFileName) // Set the default file name
-
-        val dirPath = getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath
-        val newFilename = filenameInput.text.toString()
-
-        if (dirPath == null) {
-            Toast.makeText(this, "Error: Unable to access music directory", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        val filePath = "$dirPath/$newFilename.mp3"
-        val ampsPath = "$dirPath/$newFilename"
-        val timestamp = Date().time
-
-        // Ensure amplitudes and duration are defined
-        val amplitudes = waveformView.getAmplitudes()
-        val duration = elapsedTime // Assuming elapsedTime is in milliseconds
-
-        try {
-            FileOutputStream(ampsPath).use { fos ->
-                ObjectOutputStream(fos).use { out ->
-                    out.writeObject(amplitudes)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            Toast.makeText(this, "Error saving amplitudes", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        // Create a new AudioRecord and save it to the database
-        val record = AudioRecord(newFilename, filePath, timestamp, duration.toString(), ampsPath)
-
-        GlobalScope.launch {
-            db.audioRecordDao().insert(record)
-        }
 
     }
 
@@ -347,6 +341,10 @@ class MainActivity : AppCompatActivity() {
 
             tempFile.copyTo(finalFile, overwrite = true)
             tempFile.delete() // Remove the temporary file
+
+            // Wypisanie ścieżki do pliku i nazwy pliku w konsoli
+            println("Save Recording - Final File Path: $finalFilePath")
+            println("Save Recording - Current File Name: $currentFileName")
 
             mediaRecorder?.release()
             mediaRecorder = null
@@ -406,6 +404,61 @@ class MainActivity : AppCompatActivity() {
         val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
         val date = Date()
         return "recording_${dateFormat.format(date)}"
+    }
+
+    private fun uploadRecording() {
+        mAuth.signInAnonymously().addOnSuccessListener(this) { authResult ->
+            // Sprawdzenie czy nazwa pliku została ustawiona
+            if (currentFileName.isNullOrEmpty()) {
+                Toast.makeText(this, "Please save the recording first", Toast.LENGTH_LONG).show()
+                return@addOnSuccessListener
+            }
+
+            // Ustawienie referencji do Firebase Storage
+            storageReference = FirebaseStorage.getInstance().reference.child("audio").child(currentFileName!!)
+
+            // Pobranie lokalnej ścieżki do zapisanego pliku dźwiękowego
+            val filePath = getRecordingFilePath(currentFileName!!)
+
+            // Utworzenie Uri z lokalnej ścieżki pliku
+            val fileUri = Uri.fromFile(File(filePath))
+
+            // Wypisanie ścieżki do pliku i nazwy pliku w konsoli
+            println("Upload Recording - File Path: $filePath")
+            println("Upload Recording - Current File Name: $currentFileName")
+
+            // Rozpoczęcie uploadu do Firebase Storage
+            val uploadTask = storageReference.putFile(fileUri)
+
+            // Określenie typu zawartości pliku jako "audio/mpeg"
+            val metadata = storageMetadata {
+                contentType = "audio/mpeg"
+            }
+            uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                storageReference.updateMetadata(metadata)
+            }.addOnSuccessListener {
+                // Upload zakończony sukcesem
+                Toast.makeText(this, "Recording uploaded successfully", Toast.LENGTH_LONG).show()
+
+                // Pobranie URL pliku z Firebase Storage
+                storageReference.downloadUrl.addOnSuccessListener { uri ->
+                    // Tutaj możesz wykorzystać uri do przechowywania URL w bazie danych lub wykorzystania go w inny sposób
+                }.addOnFailureListener { exception ->
+                    // Obsługa błędu w przypadku niepowodzenia pobrania URL
+                    Toast.makeText(this, "Failed to get download URL: $exception", Toast.LENGTH_LONG).show()
+                }
+            }.addOnFailureListener { exception ->
+                // Obsługa błędu w przypadku niepowodzenia uploadu
+                Toast.makeText(this, "Failed to upload recording: $exception", Toast.LENGTH_LONG).show()
+            }
+        }.addOnFailureListener(this) { exception ->
+            Log.e(TAG, "signInAnonymously:FAILURE", exception)
+        }
     }
 
     override fun onDestroy() {
